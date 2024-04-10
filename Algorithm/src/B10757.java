15ec03291e99ed3746e1fdc597a649876fbc31dd
Dohwan Kim <bar0805@naver.com>
2024-04-10 19:16:57 +0900

    10757 문제풀이

---
+++ Algorithm/src/B10757.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B10757 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // BigInteger 타입으로 변경
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        // BigInteger의 add 메소드 사용
+        BigInteger result = a.add(b);
+
+        System.out.println(result);
+    }
+}
