c674a2333eea3d52d619653e0504857c2063ee87
Dohwan Kim <bar0805@naver.com>
2024-04-30 14:49:51 +0900

    11720번 문제풀이

---
+++ Algorithm/src/BJ/B11720.java
@@ -0,0 +1,14 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class B11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int[] arr = new int[a];
+        for(int   )
+    }
+}
