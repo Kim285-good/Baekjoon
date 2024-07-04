586ebb10cca3d2ea06352fb1b543d1e73a3f5026
Dohwan Kim <bar0805@naver.com>
2024-07-04 12:01:50 +0900

    로컬 변경 사항 커밋

--- Algorithm/src/BJ/B11720.java
+++ Algorithm/src/BJ/B11720.java
@@ -5,10 +5,5 @@ import java.util.Scanner;
 public class B11720 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int a = scanner.nextInt();
-
-        int[] arr = new int[a];
-        for(int   )
     }
 }
